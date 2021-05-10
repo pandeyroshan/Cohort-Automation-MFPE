07858ffcb7fb11ee93af0f64e89b67ccb3aee301
pandeyroshan <pandeyroshan556@gmail.com>
2021-05-10 22:46:32 +0530

    MailService - Added sendCredentials

--- src/main/java/com/cohortautomation/utilities/MailService.java
+++ src/main/java/com/cohortautomation/utilities/MailService.java
@@ -1,5 +1,68 @@
 package com.cohortautomation.utilities;
 
-public class MailService {
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.cohortautomation.beans.User;
+
+public class MailService {
+	static String to = "pandeyroshan556@gmail.com";
+	static String from = "roshanpandey11022000@gmail.com";
+	static String host = "smtp.gmail.com";
+	static Properties properties = System.getProperties();
+	static Session session;
+	MimeMessage message;
+
+	public static void setup() {
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.ssl.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+
+		session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+			protected PasswordAuthentication getPasswordAuthentication() {
+
+				return new PasswordAuthentication("roshanpandey11022000@gmail.com", "SomeThing@Inn0vative");
+
+			}
+
+		});
+
+		session.setDebug(true);
+	}
+
+	public static void sendCredentials(User user) {
+		setup();
+
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getPersonalEmail()));
+
+			message.setSubject("Login Credentials for Cohort Automation");
+
+			message.setText("Hi " + user.getFirstName()
+					+ ", welcome to cohort automation. Here are your login details - \nUsername: " + user.getUsername()
+					+ "\nPassword: " + user.getPassword()
+					+ "\nUse these crendentials to login, and please change your password immediately after you first login.\nRegards,\nTeam Cohort Automation");
+
+			Transport.send(message);
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		User user = new User(909102, "909102", "Linux@123", "Roshan", "Pandey", "pandeyroshan556@gmail.com");
+
+		sendCredentials(user);
+	}
 }
--- src/main/java/com/cohortautomation/utilities/PasswordGenerator.java
+++ src/main/java/com/cohortautomation/utilities/PasswordGenerator.java
@@ -1,5 +1,29 @@
 package com.cohortautomation.utilities;
 
 public class PasswordGenerator {
-
+	public static String getRandomPassword(int length) {
+		char[] lowercaseAlphabets = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		char[] upperaseAlphabets = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+		char[] specialCharacters = {'!','$','#','%','@','*','&'};
+		char[] numbers = {'1','2','3','4','5','6','7','8','9','0'};
+		
+		String password = "";
+		
+		for(int i=0; i<length; i++)
+		{
+			int characterArrayChoice = (int) (Math.random()*100)%4;
+			
+			if(characterArrayChoice == 0) {
+				password += Character.toString(lowercaseAlphabets[(int) (Math.random()*100)%26]);
+			} else if(characterArrayChoice == 1) {
+				password += Character.toString(upperaseAlphabets[(int) (Math.random()*100)%26]);
+			} else if(characterArrayChoice == 2) {
+				password += Character.toString(specialCharacters[(int) (Math.random()*100)%7]);
+			} else if(characterArrayChoice == 3) {
+				password += Character.toString(numbers[(int) (Math.random()*100)%10]);
+			}
+		}
+		
+		return password;
+	}
 }
